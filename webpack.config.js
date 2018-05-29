e8b0d128eba1d29dde35b30448d1253879511337
hualei03042013@163.com <hualei03042013@163.com>
2018-05-30 01:26:40 +0800

    build:update sth. for pro

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,50 @@
 var path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+
+var isProduction = process.env.NODE_ENV === 'production';
+console.log(isProduction)
+console.log(process.env)
+
+var minify = isProduction ? 'minimize' : '';
+var minPostfix = isProduction ? '.min' : '';
+var hash = '[hash:7]';
+var cssLoader = [{
+		loader: 'style-loader'
+	},
+	{
+		loader: 'css-loader',
+		options: {
+			modules: false
+		}
+	},
+	{
+		loader: 'postcss-loader',
+		options: {
+			ident: 'postcss',
+			plugins: [
+				require('autoprefixer')({
+					broswer: 'last 5 versions'
+				})
+			]
+		}
+	}
+];
+var lessLoader = cssLoader.concat({
+	loader: 'less-loader'
+});
+//console.log(lessLoader)
+
+cssLoader = isProduction ? ExtractTextPlugin.extract({
+	fallback: 'style-loader',
+	use: cssLoader.shift(),
+}) : cssLoader;
+lessLoader = isProduction ? ExtractTextPlugin.extract({
+	fallback: 'style-loader',
+	use: lessLoader.shift(),
+}) : lessLoader;
+
 module.exports = {
 	entry: {
 		app: ["./app/main.js"]
@@ -10,66 +54,31 @@ module.exports = {
 		publicPath: "/assets/",
 		filename: "bundle.js"
 	},
-	plugins: [new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'app/index.html'),
-			filename: 'index.html',
-			inject: 'body'
-		}),
-		//new ExtractTextPlugin("styles.css"),
-	],
+	plugins: [
+	new webpack.DefinePlugin({
+		'process.env': {
+			'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		}
+	}),
+	new ExtractTextPlugin(`css/style.${hash}${minPostfix}.css`, {
+		allChunks: true
+	}),
+	new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, 'app/index.html'),
+		filename: 'index.html',
+		inject: 'body'
+	})],
 	module: {
 		rules: [{
 				test: /\.css$/,
 				exclude: /node_modules/,
-				use: [{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: false
-						}
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: [
-								require('autoprefixer')({
-									broswer: 'last 5 versions'
-								})
-							]
-						}
-					}
-				]
+				use: cssLoader
 			},
 			{
 				test: /\.less$/,
-				use: [{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: false
-						}
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: [
-								require('autoprefixer')({
-									broswer: 'last 5 versions'
-								})
-							]
-						}
-					},
-					{
-						loader: 'less-loader'
-					}
-				]
+				exclude: /node_modules/,
+				use: lessLoader
 			}
 		]
 	}
-};
+}
