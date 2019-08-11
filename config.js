3a7b3c9631ac69571b8bc76c6d54e2a507d38218
yemiancheng <ymc.github@gmail.com>
2019-08-11 16:34:53 +0800

    build(*): updating webpack to 4.x

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
-var path = require('path');
+const path = require('path');
 module.exports = {
   dev: {
-    //环境标识
-    env: "development",
+    // 环境标识
+    env: 'development',
 
-    //网络定位
-    //域名
+    // 网络定位
+    // 域名
     host: 'http://localhost',
-    //端口
+    // 端口
     port: 8080,
-    //路径
+    // 路径
     path: '/',
-    //文件
+    // 文件
 
 
-    //物理定位
-    //目录
-    dist:'dist',   
-    //静态资源目录
+    // 物理定位
+    // 目录
+    dist: 'dist',
+    // 静态资源目录
     assetsSubDirectory: 'static',
-    //静态资源根录
+    // 静态资源根录
     assetsPublicPath: '/',
 
-    //代理
+    // 代理
     proxyTable: {},
-    //样式资源映射
+    // 样式资源映射
     cssSourceMap: false
   }
-}
+};
